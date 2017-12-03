25aa54e6f89aa9cf95299c2651dc8590f6b9e48c
Riley Flynn <riley@rileyflynn.me>
2017-12-03 13:40:13 -0330

    Completed Day 1

---
+++ 2017/Day1_Captcha_Part1.py
@@ -0,0 +1,13 @@
+def solve_captcha(captcha: str) -> int:
+    last = None
+    total = 0
+    for char in captcha:
+        char = int(char)
+        if char == last:
+            total += char
+        last = char
+    if len(captcha) > 2:
+        if captcha[-1] == captcha[0]:
+            total += int(captcha[0])
+
+    return total
