27a44503bd9712bdf3233533dbff1a1e6125db6b
Riley Flynn <riley@rileyflynn.me>
2023-11-15 20:40:16 -0330

    Temp code changes, to test formatting

--- 2022/06/Day6.fsx
+++ 2022/06/Day6.fsx
@@ -5,8 +5,7 @@ let findMarker (size: int) (input: string) : int =
     |> Array.ofSeq
     |> Array.windowed size
     |> Array.map (fun window -> (window |> Set.ofArray |> Set.count))
-    |> Array.findIndex (fun uniqueCount -> uniqueCount = size)
-    |> (+) size
+    |> Array.findIndex (fun uniqueCount -> uniqueCount = size) |> (+) size
 
 printfn $"%A{findMarker 4 inputData}"
 printfn $"%A{findMarker 14 inputData}"
