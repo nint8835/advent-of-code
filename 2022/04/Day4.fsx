46b1b25056c81a753702f4086bbc3d16b50ab25e
Riley Flynn <riley@rileyflynn.me>
2024-11-14 20:06:59 -0330

    Slight 2022/04 cleanup

--- 2022/04/Day4.fsx
+++ 2022/04/Day4.fsx
@@ -1,7 +1,15 @@
+#load "../../utils/Utils.fsx"
+
+open System.IO
+open Utils
+
 let inputData =
-    (System.IO.File.ReadAllText "input.txt").Split "\n"
+    File.ReadAllText "input.txt"
+    |> String.split "\n"
     |> Array.map (fun line ->
-        line.Split "," |> Array.map (fun elf -> elf.Split "-" |> Array.map int))
+        line
+        |> String.split ","
+        |> Array.map (fun elf -> elf |> String.split "-" |> Array.map int))
 
 let tryBothOrders (func: int[] -> int[] -> bool) (pair: int[][]) : bool =
     func pair[0] pair[1] || func pair[1] pair[0]
@@ -10,11 +18,6 @@ let partA (a: int[]) (b: int[]) : bool = a[0] <= b[0] && a[1] >= b[1]
 
 let partB (a: int[]) (b: int[]) : bool = a[0] <= b[0] && b[0] <= a[1]
 
-let partAResult =
-    inputData |> Array.filter (tryBothOrders partA) |> Array.length
 
-let partBResult =
-    inputData |> Array.filter (tryBothOrders partB) |> Array.length
-
-printfn $"%A{partAResult}"
-printfn $"%A{partBResult}"
+inputData |> Array.filter (tryBothOrders partA) |> Array.length |> printfn "%A"
+inputData |> Array.filter (tryBothOrders partB) |> Array.length |> printfn "%A"
