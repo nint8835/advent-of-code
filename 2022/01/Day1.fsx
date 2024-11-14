34ea8cdeaa477ae4a92d01934b2aaee85e30227e
Riley Flynn <riley@rileyflynn.me>
2024-11-14 19:35:35 -0330

    Minor cleanup 2022/01

--- 2022/01/Day1.fsx
+++ 2022/01/Day1.fsx
@@ -1,13 +1,19 @@
-let inputData =
-    (System.IO.File.ReadAllText "input.txt").Split "\n\n"
-    |> Array.map (fun elf -> elf.Split "\n" |> Array.map int)
+#load "../../utils/Utils.fsx"
 
-let maxNElves (elves: int[][]) (n: int) : int =
+open System.IO
+open Utils
+
+let inputData =
+    File.ReadAllText "input.txt"
+    |> String.split "\n\n"
+    |> Array.map (fun elf -> elf |> String.split "\n" |> Array.map int)
+
+let maxNElves (n: int) (elves: int[][]) : int =
     elves
     |> Array.map Array.sum
     |> Array.sortDescending
     |> Array.take n
     |> Array.sum
 
-printfn $"Part A: {maxNElves inputData 1}"
-printfn $"Part B: {maxNElves inputData 3}"
+inputData |> maxNElves 1 |> printfn "Part A: %d"
+inputData |> maxNElves 3 |> printfn "Part B: %d"
