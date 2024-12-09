27eed8dd41dbe2c8b9c95dccfca6ce12dce614a5
Riley Flynn <riley@rileyflynn.me>
2024-12-09 12:04:15 -0330

    Unusably slow part 2
    Ran for 3 hours, never actually got an answer

--- 2024/09/Day9.fsx
+++ 2024/09/Day9.fsx
@@ -12,8 +12,6 @@ let fileSizes =
     |> Seq.map (fun (i, d) -> (i / 2, int d))
     |> Map.ofSeq
 
-fileSizes |> printfn "%A"
-
 let inputData =
     inputText
     |> Seq.mapi (fun i d ->
@@ -38,6 +36,42 @@ let rec moveBlocksPtA (input: int option[]) : int option[] =
 
         newArray |> moveBlocksPtA
 
+let moveBlocksPtB (input: int option[]) (target: int) : int option[] =
+    let availableGaps =
+        input
+        |> Array.indexed
+        |> Array.filter (fun (i, v) -> v |> Option.isNone)
+        |> Array.filter (fun (i, _) ->
+            i < (input |> Array.findIndex (fun v -> v = Some target)))
+        |> Array.map fst
+        |> Array.map (fun i ->
+            (i,
+             input
+             |> Array.skip i
+             |> Array.takeWhile Option.isNone
+             |> Array.length))
+
+    let targetSize = fileSizes |> Map.find target
+
+    let targetGap =
+        availableGaps |> Array.tryFind (fun (_, gap) -> gap >= targetSize)
+
+    match targetGap with
+    | None -> input
+    | Some(i, _) ->
+        let targetRemovedArr =
+            input
+            |> Array.mapi (fun j v ->
+                match v with
+                | Some t when t = target -> None
+                | _ -> v)
+
+        [| i .. (i + targetSize - 1) |]
+        |> Array.fold
+            (fun arr j -> arr |> Array.updateAt j (Some target))
+            targetRemovedArr
+
+
 let partA =
     inputData
     |> moveBlocksPtA
@@ -45,4 +79,17 @@ let partA =
     |> Array.mapi (fun i v -> uint64 i * uint64 v)
     |> Array.sum
 
+let partB =
+    fileSizes
+    |> Map.keys
+    |> Seq.toArray
+    |> Array.sortDescending
+    |> Array.fold moveBlocksPtB inputData
+    |> Array.indexed
+    |> Array.filter (fun (_, v) -> v |> Option.isSome)
+    |> Array.map (fun (i, v) -> (uint64 i, uint64 (v |> Option.get)))
+    |> Array.map (fun (i, v) -> i * v)
+    |> Array.sum
+
 partA |> printfn "%d"
+partB |> printfn "%d"
