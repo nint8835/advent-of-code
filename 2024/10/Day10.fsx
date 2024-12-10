0819f3f9a3e2ae0d6b7c627aaa0858c6e808aa8c
Riley Flynn <riley@rileyflynn.me>
2024-12-10 14:57:13 -0330

    2024 Day 10

---
+++ 2024/10/Day10.fsx
@@ -0,0 +1,41 @@
+#load "../../utils/Utils.fsx"
+
+open System.IO
+open Utils
+
+let inputData =
+    File.ReadAllLines "input.txt"
+    |> Array.map (Seq.map (string >> int))
+    |> array2D
+
+let inputWidth, inputHeight = inputData.GetLength 1, inputData.GetLength 0
+
+let rec findPeaks ((x, y): int * int) : Set<int * int> * int =
+    let currentValue = inputData.[y, x]
+
+    if currentValue = 9 then
+        Set.singleton (x, y), 1
+    else
+        [| (0, -1); (0, 1); (-1, 0); (1, 0) |]
+        |> Array.map (fun (dx, dy) -> (x + dx, y + dy))
+        |> Array.filter (fun (nx, ny) ->
+            nx >= 0 && nx < inputWidth && ny >= 0 && ny < inputHeight)
+        |> Array.map (fun (nx, ny) -> ((nx, ny), inputData.[ny, nx]))
+        |> Array.filter (fun (_, v) -> (v - currentValue) = 1)
+        |> Array.map fst
+        |> Array.map findPeaks
+        |> Array.fold
+            (fun (setAcc, countAcc) (set, count) ->
+                Set.union setAcc set, countAcc + count)
+            (Set.empty, 0)
+
+let searchResult =
+    inputData |> Array2D.findIndices ((=) 0) |> Array.map findPeaks
+
+searchResult
+|> Array.map fst
+|> Array.map Set.count
+|> Array.sum
+|> printfn "%A"
+
+searchResult |> Array.map snd |> Array.sum |> printfn "%A"
