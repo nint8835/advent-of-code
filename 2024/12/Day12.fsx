39084ee4852e37d7f1de7d7a7b5c66bc22d05c8f
Riley Flynn <riley@rileyflynn.me>
2024-12-12 20:12:14 -0330

    2024, day 12, part 2

--- 2024/12/Day12.fsx
+++ 2024/12/Day12.fsx
@@ -10,22 +10,25 @@ let inputData =
 
 let inputWidth, inputHeight = inputData.GetLength 1, inputData.GetLength 0
 
-type PlotSize = { Perimeter: int; Area: int }
+type Plot =
+    { Perimeter: int
+      Area: int
+      Coordinates: Set<int * int> }
 
 [<TailCall>]
 let rec discoverPlotSize
     (targetPoints: (int * int)[])
     (seenPoints: Set<int * int>)
-    (currentSize: PlotSize)
-    : Set<int * int> * PlotSize =
+    (currentPlot: Plot)
+    : Set<int * int> * Plot =
     if targetPoints |> Array.length = 0 then
-        seenPoints, currentSize
+        seenPoints, currentPlot
     else
         let target = targetPoints |> Array.head
         let remainingPoints = targetPoints |> Array.skip 1
 
         if seenPoints |> Set.contains target then
-            discoverPlotSize remainingPoints seenPoints currentSize
+            discoverPlotSize remainingPoints seenPoints currentPlot
         else
             let (x, y) = target
             let targetValue = inputData.[y, x]
@@ -40,25 +43,66 @@ let rec discoverPlotSize
             discoverPlotSize
                 (remainingPoints |> Array.append samePlants)
                 (seenPoints |> Set.add target)
-                { Perimeter = currentSize.Perimeter + 4 - samePlants.Length
-                  Area = currentSize.Area + 1 }
+                { Perimeter = currentPlot.Perimeter + 4 - samePlants.Length
+                  Area = currentPlot.Area + 1
+                  Coordinates = currentPlot.Coordinates |> Set.add target }
 
-[| 0 .. inputHeight - 1 |]
-|> Array.map (fun y -> [| 0 .. inputWidth - 1 |] |> Array.map (fun x -> (x, y)))
-|> Array.concat
-|> Array.fold
-    (fun (seenPoints, plots) target ->
-        if seenPoints |> Set.contains target then
-            seenPoints, plots
-        else
-            let (newSeenPoints, plotSize) =
-                discoverPlotSize
-                    [| target |]
-                    seenPoints
-                    { Perimeter = 0; Area = 0 }
+let countCorners (plot: Plot) : int =
+    let outerCorners =
+        plot.Coordinates
+        |> Set.toArray
+        |> Array.map (fun (x, y) ->
+            [| [| (x - 1, y); (x, y - 1) |]
+               [| (x, y - 1); (x + 1, y) |]
+               [| (x - 1, y); (x, y + 1) |]
+               [| (x, y + 1); (x + 1, y) |] |]
+            |> Array.filter (fun coords ->
+                coords
+                |> Array.forall (fun coord ->
+                    not (plot.Coordinates |> Set.contains coord)))
+            |> Array.length)
+        |> Array.sum
 
-            newSeenPoints, plots |> Array.append [| plotSize |])
-    (Set.empty, [||])
-|> snd
-|> Array.sumBy (fun plot -> plot.Area * plot.Perimeter)
-|> printfn "%A"
+    let innerCorners =
+        plot.Coordinates
+        |> Set.toArray
+        |> Array.map (fun (x, y) ->
+            [| ([| (x - 1, y); (x, y - 1) |], (x - 1, y - 1))
+               ([| (x, y - 1); (x + 1, y) |], (x + 1, y - 1))
+               ([| (x - 1, y); (x, y + 1) |], (x - 1, y + 1))
+               ([| (x, y + 1); (x + 1, y) |], (x + 1, y + 1)) |]
+            |> Array.filter (fun (coords, diag) ->
+                coords
+                |> Array.forall (fun coord ->
+                    (plot.Coordinates |> Set.contains coord))
+                && not (plot.Coordinates |> Set.contains diag))
+            |> Array.length)
+        |> Array.sum
+
+    innerCorners + outerCorners
+
+let plots =
+    [| 0 .. inputHeight - 1 |]
+    |> Array.map (fun y ->
+        [| 0 .. inputWidth - 1 |] |> Array.map (fun x -> (x, y)))
+    |> Array.concat
+    |> Array.fold
+        (fun (seenPoints, plots) target ->
+            if seenPoints |> Set.contains target then
+                seenPoints, plots
+            else
+                let (newSeenPoints, plotSize) =
+                    discoverPlotSize
+                        [| target |]
+                        seenPoints
+                        { Perimeter = 0
+                          Area = 0
+                          Coordinates = Set.empty }
+
+                newSeenPoints, plots |> Array.append [| plotSize |])
+        (Set.empty, [||])
+    |> snd
+
+plots |> Array.sumBy (fun plot -> plot.Area * plot.Perimeter) |> printfn "%A"
+
+plots |> Array.sumBy (fun plot -> plot.Area * countCorners plot) |> printfn "%A"
