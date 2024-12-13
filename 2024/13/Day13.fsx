a9d0ae76d9678e900bffa9f8d45192d053b226df
Riley Flynn <riley@rileyflynn.me>
2024-12-13 09:19:37 -0330

    Improved day 13 part 1, non-functioning part 2

--- 2024/13/Day13.fsx
+++ 2024/13/Day13.fsx
@@ -7,9 +7,25 @@ open System.Text.RegularExpressions
 let coordinateRegex = Regex(@"[+=](\d+)")
 
 type Machine =
-    { A: int * int
-      B: int * int
-      Prize: int * int }
+    { A: int64 * int64
+      B: int64 * int64
+      Prize: int64 * int64 }
+
+let solveSystem
+    (a: int64 * int64)
+    (b: int64 * int64)
+    (prize: int64 * int64)
+    : int64 * int64 =
+    let x1, x2 = fst a, fst b
+    let y1, y2 = snd a, snd b
+    let pX, pY = fst prize, snd prize
+
+    let det = x1 * y2 - x2 * y1
+    let detX = pX * y2 - x2 * pY
+    let detY = x1 * pY - pX * y1
+
+    detX / det, detY / det
+
 
 let inputData =
     File.ReadAllText "input.txt"
@@ -21,26 +37,29 @@ let inputData =
             |> Array.map (fun line ->
                 let matches = coordinateRegex.Matches line
 
-                (int matches[0].Groups.[1].Value,
-                 int matches[1].Groups.[1].Value))
+                (int64 matches[0].Groups.[1].Value,
+                 int64 matches[1].Groups.[1].Value))
 
         { A = a; B = b; Prize = prize })
 
-inputData
-|> Array.map (fun machine ->
-    [| 0..100 |]
-    |> Array.map (fun aPresses ->
-        [| 0..100 |] |> Array.map (fun bPresses -> (aPresses, bPresses)))
-    |> Array.concat
-    |> Array.filter (fun (aPresses, bPresses) ->
-        (fst machine.A * aPresses + fst machine.B * bPresses) = fst
+let findTokens (prizeCoordinateMutator: int64 -> int64) : int =
+    inputData
+    |> Array.map (fun machine ->
+        solveSystem
+            machine.A
+            machine.B
+            (prizeCoordinateMutator (fst machine.Prize),
+             prizeCoordinateMutator (snd machine.Prize)),
+        machine)
+    |> Array.filter (fun ((aPresses, bPresses), machine) ->
+        aPresses >= 0L
+        && bPresses >= 0L
+        && (aPresses * fst machine.A + bPresses * fst machine.B) = fst
             machine.Prize
-        && (snd machine.A * aPresses + snd machine.B * bPresses) = snd
+        && (aPresses * snd machine.A + bPresses * snd machine.B) = snd
             machine.Prize)
-    |> Array.map (fun (aPresses, bPresses) -> 3 * aPresses + bPresses)
-    |> Array.sort
-    |> Array.tryHead)
-|> Array.filter Option.isSome
-|> Array.map Option.get
-|> Array.sum
-|> printfn "%A"
+    |> Array.sumBy (fun ((aPresses, bPresses), _) ->
+        int (3L * aPresses + bPresses))
+
+findTokens id |> printfn "%d"
+findTokens (fun v -> v + 10000000000000L) |> printfn "%d"
