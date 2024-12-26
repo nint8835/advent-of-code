23f160af47c89ad49f999e6de8f4535e34a0ce12
Riley Flynn <riley@rileyflynn.me>
2024-12-26 17:47:43 -0330

    WIP solver

---
+++ 2024/01/Day1.fs
@@ -0,0 +1,4 @@
+module AdventOfCode.Solutions.Y2024.D01
+
+let salve() : unit =
+    failwith "Not implemented"
--- runner/Program.fs
+++ runner/Program.fs
@@ -1,7 +1,19 @@
-﻿open System
+﻿open System.IO
+open System.Reflection
 
 [<EntryPoint>]
 let main args =
     let targetFile = args |> Array.head
+    let dayFolder = targetFile |> Directory.GetParent
+    let yearFolder = dayFolder.Parent
+    
+    let year = yearFolder.Name
+    let day = dayFolder.Name
+    
+    let solutionAssembly = Assembly.Load "AdventOfCode.Solutions"
+    let solutionType = $"AdventOfCode.Solutions.Y%s{year}.D%s{day}" |> solutionAssembly.GetType
+    let solveMethod = solutionType.GetMethod "solve"
+    
+    solveMethod.Invoke(null, [||]) |> ignore
 
     0
