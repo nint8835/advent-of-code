b3f6fecbd9b2f34ba1d05d3be8345bae0bc62cad
Riley Flynn <riley@rileyflynn.me>
2025-12-05 08:45:12 -0330

    2025 Day 5

---
+++ 2025/05/Day5.fs
@@ -0,0 +1,39 @@
+module AdventOfCode.Solutions.Y2025.D05
+
+open System
+open System.IO
+open AdventOfCode.Solutions.Utils
+
+let (rangeLines, ingredientLines) =
+    File.ReadAllText "input.txt"
+    |> String.split "\n\n"
+    |> Array.map (String.split "\n")
+    |> Array.toTuple2
+
+let freshIngredients =
+    rangeLines
+    |> Array.map (String.split "-" >> Array.map int64 >> Array.toTuple2)
+
+let availableIngredients = ingredientLines |> Array.map int64
+
+let solve () =
+    availableIngredients
+    |> Array.filter (fun v ->
+        (freshIngredients
+         |> Array.exists (fun (min, max) -> min <= v && max >= v)))
+    |> Array.length
+    |> printfn "%d"
+
+    freshIngredients
+    |> Array.sortBy fst
+    |> Array.fold
+        (fun acc (min, max) ->
+            match acc with
+            | [] -> [ (min, max) ]
+            | (lastMin, lastMax) :: tail when min <= lastMax + 1L ->
+                (lastMin, Math.Max(lastMax, max)) :: tail
+            | _ -> (min, max) :: acc)
+        []
+    |> List.map (fun (min, max) -> max - min + 1L)
+    |> List.sum
+    |> printfn "%d"
